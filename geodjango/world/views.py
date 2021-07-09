95cc5b1645f7fa30cc72bb61ba12dfd6595f30d2
Pavan Kumar Reddy <56450475+bot977@users.noreply.github.com>
2021-07-09 21:22:54 +0530

    added qgis path to environment variables

--- geodjango/world/views.py
+++ geodjango/world/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, redirect
 from geo.Geoserver import Geoserver
 from .models import Sentinel
 import sys
+import json
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Create your views here.
 def index(request):
@@ -14,7 +20,8 @@ def index(request):
 def cluster(request, id):
 
     # QGIS Application
-    # path = ['C:/OSGeo4W/apps/qgis-ltr/./python', 'C:/Users/YVM Reddy/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'C:/Users/YVM Reddy/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python/plugins', 'C:/OSGeo4W/apps/qgis-ltr/./python/plugins', 'C:\\Program Files\\QGIS 3.18\\apps\\qgis\\python', 'C:\\OSGeo4W\\bin\\python39.zip', 'C:\\OSGeo4W\\apps\\Python39\\DLLs', 'C:\\OSGeo4W\\apps\\Python39\\lib', 'C:\\OSGeo4W\\bin', 'C:\\Users\\YVM Reddy\\AppData\\Roaming\\Python\\Python39\\site-packages', 'C:\\OSGeo4W\\apps\\Python39', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages\\win32', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages\\win32\\lib', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages\\Pythonwin', 'C:/Users/YVM Reddy/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python']
+    path = env.list('QGIS_PATH')
+    print(path[0])
     # for i in path:
     #     sys.path.append(i)
         
