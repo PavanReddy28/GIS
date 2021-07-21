57dd51c259e69a1d81b156f0051d507286b6801d
yashpat153 <yypathak35@gmail.com>
2021-07-21 15:49:11 +0530

    cluster-change functions working

--- geodjango/world/qgis_cluster.py
+++ geodjango/world/qgis_cluster.py
@@ -1,9 +1,13 @@
 import sys
 
 def perform_cluster(input_path, output_path):
-    path = ['C:/OSGeo4W/apps/qgis-ltr/./python', 'C:/Users/YVM Reddy/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'C:/Users/YVM Reddy/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python/plugins', 'C:/OSGeo4W/apps/qgis-ltr/./python/plugins', 'C:\\Program Files\\QGIS 3.18\\apps\\qgis\\python', 'C:\\OSGeo4W\\bin\\python39.zip', 'C:\\OSGeo4W\\apps\\Python39\\DLLs', 'C:\\OSGeo4W\\apps\\Python39\\lib', 'C:\\OSGeo4W\\bin', 'C:\\Users\\YVM Reddy\\AppData\\Roaming\\Python\\Python39\\site-packages', 'C:\\OSGeo4W\\apps\\Python39', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages\\win32', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages\\win32\\lib', 'C:\\OSGeo4W\\apps\\Python39\\lib\\site-packages\\Pythonwin', 'C:/Users/YVM Reddy/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python']
-    print(path[0], path[1])
-    for i in path:
+    #path modification to disable the virtual env
+    prev_sys_path = sys.path
+    prev_executable_path  = sys.executable
+    sys.path = ['C:\\Users\\Admin\\Desktop', 'c:\\users\\admin\\appdata\\local\\programs\\python\\python39\\python39.zip', 'c:\\users\\admin\\appdata\\local\\programs\\python\\python39\\DLLs', 'c:\\users\\admin\\appdata\\local\\programs\\python\\python39\\lib', 'c:\\users\\admin\\appdata\\local\\programs\\python\\python39']
+    sys.executable = r"C:\Users\Admin\AppData\Local\Programs\Python\Python39\python.exe"
+    path_list = ['C:/PROGRA~1/QGIS32~1.0/apps/qgis/./python', 'C:/Users/Admin/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python', 'C:/Users/Admin/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python/plugins', 'C:/PROGRA~1/QGIS32~1.0/apps/qgis/./python/plugins', 'C:\\Program Files\\QGIS 3.20.0\\bin\\python39.zip', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39\\DLLs', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39\\lib', 'C:\\Program Files\\QGIS 3.20.0\\bin', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39\\lib\\site-packages', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39\\lib\\site-packages\\win32', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39\\lib\\site-packages\\win32\\lib', 'C:\\PROGRA~1\\QGIS32~1.0\\apps\\Python39\\lib\\site-packages\\Pythonwin', 'C:/Users/Admin/AppData/Roaming/QGIS/QGIS3\\profiles\\default/python'] 
+    for i in path_list:
         sys.path.append(i)
 
     from qgis.core import QgsApplication, QgsRasterLayer, QgsProcessingFeedback
@@ -17,9 +21,9 @@ def perform_cluster(input_path, output_path):
 
     Processing.initialize()
     input_raster = QgsRasterLayer(input_path,'olayer')
-    output_path = output_path
+    out_path = output_path
     cluster_params = {
-        'CLUSTER':output_path,
+        'CLUSTER':out_path,
         'GRIDS':[input_raster],
         'METHOD':1,
         'NCLUSTER':5,
@@ -54,3 +58,7 @@ def perform_cluster(input_path, output_path):
     processing.run("saga:kmeansclusteringforrasters",cluster_params,feedback=MyFeedBack())
 
     qgs.exitQgis()
+
+    #restore path variables
+    sys.path = prev_sys_path
+    sys.executable = prev_executable_path
